ce104d3f89c41021018bc8865094f264fac4391b
Jonathan Thuau <jonathan.thuau@gmail.com>
2014-06-05 12:14:29 +0200

    Update long_url pattern

--- src/Thujohn/Twitter/Twitter.php
+++ src/Thujohn/Twitter/Twitter.php
@@ -75,7 +75,7 @@ class Twitter extends tmhOAuth {
 		$patterns['mailto']   = '(([a-z0-9_]|\\-|\\.)+@([^[:space:]]*)([[:alnum:]-]))';
 		$patterns['user']     = ' +@([a-z0-9_]*)?';
 		$patterns['hashtag']  = ' +#([a-z0-9_\p{Cyrillic}\d]*)?';
-		$patterns['long_url'] = '>(([[:alnum:]]+:\/\/)|www\.)?([^[:space:]]{10,20})([^[:space:]]*)([^[:space:]]{10,20})([[:alnum:]#?\/&=])<';
+		$patterns['long_url'] = '>(([[:alnum:]]+:\/\/)|www\.)?([^[:space:]]{12,22})([^[:space:]]*)([^[:space:]]{12,22})([[:alnum:]#?\/&=])<';
 
 		// URL
 		$pattern = '(?xi)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
