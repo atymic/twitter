5e279da51c9912595508bd2e63e5a15de23e0e7b
Julio Foulquié <jfoulquie@gmail.com>
2014-08-23 15:19:46 +0200

    Add Twitter login URLs to config file.

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,10 @@
 return array(
 	'API_URL'             => 'api.twitter.com',
 	'API_VERSION'         => '1.1',
+	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
+	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
+	'ACCESS_TOKEN_URL'    => 'oauth/access_token',
+	'REQUEST_TOKEN_URL'   => 'oauth/request_token',
 	'USE_SSL'             => true,
 
 	'CONSUMER_KEY'        => '',
