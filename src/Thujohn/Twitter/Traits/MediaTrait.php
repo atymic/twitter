a0b2c5e2ef8a2b925e4fe7fcdf088cd3244b10a4
Jonathan Thuau <jonathan.thuau@gmail.com>
2015-04-19 18:03:15 +0200

    Update MediaTrait.php

--- src/Thujohn/Twitter/Traits/MediaTrait.php
+++ src/Thujohn/Twitter/Traits/MediaTrait.php
@@ -9,15 +9,19 @@ Trait MediaTrait {
 	 *
 	 * Parameters :
 	 * - media
+	 * - media_data
 	 */
 	public function uploadMedia($parameters = [])
 	{
-        if (!array_key_exists('media', $parameters) || !array_key_exists('media_data', $parameters)) {
-            throw new Exception('Parameter required missing : media or media_data');
-        }
-        if (array_key_exists('media', $parameters) && array_key_exists('media_data', $parameters)) {
-            throw new Exception('You cannot use media and media_data at the same time');
-        }
+	        if (!array_key_exists('media', $parameters) || !array_key_exists('media_data', $parameters))
+	        {
+	            throw new Exception('Parameter required missing : media or media_data');
+	        }
+
+	        if (array_key_exists('media', $parameters) && array_key_exists('media_data', $parameters))
+	        {
+	            throw new Exception('You cannot use media and media_data at the same time');
+	        }
 
 		return $this->post('media/upload', $parameters, true);
 	}
