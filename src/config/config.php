a0db5642891545644c84511a78893a56ae0b63c5
Tyler Youschak <tyleryouschak@gmail.com>
2015-05-04 17:22:37 -0400

    .env Support
    You can now get up and going much quicker if you use Laravel 5! Just add
    the four items to your .env file so you don't have to worry about prying
    eyes seeing your keys!

--- src/config/config.php
+++ src/config/config.php
@@ -14,8 +14,8 @@ return [
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => '',
-	'CONSUMER_SECRET'     => '',
-	'ACCESS_TOKEN'        => '',
-	'ACCESS_TOKEN_SECRET' => '',
+	'CONSUMER_KEY'        => env('TWITTER_CONSUMER_KEY', ''),
+	'CONSUMER_SECRET'     => env('TWITTER_CONSUMER_SECRET', ''),
+	'ACCESS_TOKEN'        => env('TWITTER_ACCESS_TOKEN', ''),
+	'ACCESS_TOKEN_SECRET' => env('TWITTER_ACCESS_TOKEN_SECRET', ''),
 ];
