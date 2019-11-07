c1e146e3329ea1e1c0a8fd64a002d6f2757864ef
atymic <atymicq@gmail.com>
2019-11-07 21:34:59 +1100

    fix: fix regex escaping issue in php7.3
    See https://github.com/thujohn/twitter/issues/250

--- src/Thujohn/Twitter/Twitter.php
+++ src/Thujohn/Twitter/Twitter.php
@@ -313,7 +313,7 @@ class Twitter extends tmhOAuth
         $patterns['url'] = '(?xi)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
         $patterns['mailto'] = '([_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3}))';
         $patterns['user'] = ' +@([a-z0-9_]*)?';
-        $patterns['hashtag'] = '(?:(?<=\s)|^)#(\w*[\p{L}-\d\p{Cyrillic}\d]+\w*)';
+        $patterns['hashtag'] = '(?:(?<=\s)|^)#(\w*[\p{L}\-\d\p{Cyrillic}\d]+\w*)';
         $patterns['long_url'] = '>(([[:alnum:]]+:\/\/)|www\.)?([^[:space:]]{12,22})([^[:space:]]*)([^[:space:]]{12,22})([[:alnum:]#?\/&=])<';
 
         if ($type == 'text') {
