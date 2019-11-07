57b971749c298d56fc025f8ce9a9dd584edd49b9
atymic <atymicq@gmail.com>
2019-11-07 09:49:59 +0000

    Apply fixes from StyleCI

--- src/config/config.php
+++ src/config/config.php
@@ -3,19 +3,19 @@
 // You can find the keys here : https://apps.twitter.com/
 
 return [
-	'debug'               => function_exists('env') ? env('APP_DEBUG', false) : false,
+    'debug'               => function_exists('env') ? env('APP_DEBUG', false) : false,
 
-	'API_URL'             => 'api.twitter.com',
-	'UPLOAD_URL'          => 'upload.twitter.com',
-	'API_VERSION'         => '1.1',
-	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
-	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
-	'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
-	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
-	'USE_SSL'             => true,
+    'API_URL'             => 'api.twitter.com',
+    'UPLOAD_URL'          => 'upload.twitter.com',
+    'API_VERSION'         => '1.1',
+    'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
+    'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
+    'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
+    'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
+    'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
-	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
-	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
-	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
+    'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
+    'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
+    'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
+    'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
 ];
