303edbeb094428d1d3f2351ef91f67ddbe0e3cf3
atymic <atymicq@gmail.com>
2019-11-08 15:03:59 +1100

    fix: support for new dm api endpoints

--- src/Thujohn/Twitter/Traits/DirectMessageTrait.php
+++ src/Thujohn/Twitter/Traits/DirectMessageTrait.php
@@ -7,22 +7,7 @@ use BadMethodCallException;
 trait DirectMessageTrait
 {
     /**
-     * Returns the 20 most recent direct messages sent by the authenticating user. Includes detailed information about the sender and recipient user. You can request up to 200 direct messages per call, up to a maximum of 800 outgoing DMs.
-     *
-     * Parameters :
-     * - since_id
-     * - max_id
-     * - count (1-200)
-     * - page
-     * - include_entities (0|1)
-     */
-    public function getDmsOut($parameters = [])
-    {
-        return $this->get('direct_messages/sent', $parameters);
-    }
-
-    /**
-     * Returns a single direct message, specified by an id parameter. Like the /1.1/direct_messages.format request, this method will include the user objects of the sender and recipient.
+     * Returns a single direct message eveny, specified by an id parameter.
      *
      * Parameters :
      * - id
@@ -33,22 +18,19 @@ trait DirectMessageTrait
             throw new BadMethodCallException('Parameter required missing : id');
         }
 
-        return $this->get('direct_messages/show', $parameters);
+        return $this->get('direct_messages/events/show', $parameters);
     }
 
     /**
-     * Returns the 20 most recent direct messages sent to the authenticating user. Includes detailed information about the sender and recipient user. You can request up to 200 direct messages per call, and only the most recent 200 DMs will be available using this endpoint.
+     * Returns all Direct Message events (both sent and received) within the last 30 days. Sorted in reverse-chronological order.
      *
      * Parameters :
-     * - since_id
-     * - max_id
-     * - count (1-200)
-     * - include_entities (0|1)
-     * - skip_status (0|1)
+     * - count (1-50)
+     * - cursor
      */
-    public function getDmsIn($parameters = [])
+    public function getDms($parameters = [])
     {
-        return $this->get('direct_messages', $parameters);
+        return $this->get('direct_messages/events/list', $parameters);
     }
 
     /**
@@ -56,7 +38,6 @@ trait DirectMessageTrait
      *
      * Parameters :
      * - id
-     * - include_entities
      */
     public function destroyDm($parameters = [])
     {
@@ -64,23 +45,24 @@ trait DirectMessageTrait
             throw new BadMethodCallException('Parameter required missing : id');
         }
 
-        return $this->post('direct_messages/destroy', $parameters);
+        return $this->delete('direct_messages/events/destroy', $parameters);
     }
 
     /**
-     * Sends a new direct message to the specified user from the authenticating user. Requires both the user and text parameters and must be a POST. Returns the sent message in the requested format if successful.
+     * Publishes a new message_create event resulting in a Direct Message sent to a specified user from the authenticating user. Returns an event if successful. Supports publishing Direct Messages with optional Quick Reply and media attachment.
      *
      * Parameters :
-     * - user_id
-     * - screen_name
-     * - text
+     * - type
+     * - message_create
+     *
+     * @see https://developer.twitter.com/en/docs/direct-messages/sending-and-receiving/api-reference/new-event
      */
     public function postDm($parameters = [])
     {
-        if ((!array_key_exists('user_id', $parameters) && !array_key_exists('screen_name', $parameters)) || !array_key_exists('text', $parameters)) {
+        if ((!array_key_exists('type', $parameters) && !array_key_exists('message_create', $parameters))) {
             throw new BadMethodCallException('Parameter required missing : user_id, screen_name or text');
         }
 
-        return $this->post('direct_messages/new', $parameters);
+        return $this->post('direct_messages/events/new', $parameters);
     }
 }
